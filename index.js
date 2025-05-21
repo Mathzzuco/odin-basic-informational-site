e137a1250ecfd4224ef87447ddce423e4c86ee10
Mathzzuco <matheusmazzuco05@gmail.com>
2025-05-21 14:36:46 -0300

    add express routes

--- index.js
+++ index.js
@@ -1,31 +1,40 @@
-const url = require('url')
-const fs = require('fs')
-const http = require('http')
+const fs = require('fs');
+const express = require('express');
+const app = express();
 
-http.createServer(function (req, res) {
-    const query = url.parse(req.url, true);
-
-    if (query.pathname == "/") {
-        var filename = "./index.html"
-    } else {
-        var filename = "." + query.pathname + ".html";
-    }
-
-    fs.readFile(filename, function(err, data) {
-    if (err) {
-        fs.readFile("./404.html", function(err, data) {
-            if (err) {
-                res.writeHead(404, {'Content-Type': 'text/html'});
-                return res.end("404 Not Found");
-            }
-            res.writeHead(404, {'Content-Type': 'text/html'});
-            res.write(data);
-            return res.end();
-        })
-    } else {
+app.get("/", (req, res) => {
+    fs.readFile("./index.html", function(err, data) {
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.write(data);
         return res.end();
-    }
-    });
-}).listen(8080);
+    })
+})
+
+app.get("/about", (req, res) => {
+    fs.readFile("./about.html", function(err, data) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+    })
+})
+
+app.get("/contact", (req, res) => {
+    fs.readFile("./contact-me.html", function(err, data) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+    })
+})
+
+app.use((req, res) => {
+    fs.readFile("./404.html", function(err, data) {
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+    })
+})
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`My first Express app - listening on port ${PORT}!`);
+});
